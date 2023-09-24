d29608d6d4b5504a21df00eb32021e543c31b14e
Harish1651999 <hk1651999@gmail.com>
2023-09-24 09:32:24 +0530

    add error handling and utility function.

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,9 +1,10 @@
 import React from "react";
 import EventsList from "../components/EventsList";
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 const EventsPage = () => {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return <EventsList events={events} />;
 };
@@ -11,12 +12,15 @@ const EventsPage = () => {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/eventsaas");
 
   if (!response.ok) {
-    // ...
+    // throw new Error(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+
+    throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
