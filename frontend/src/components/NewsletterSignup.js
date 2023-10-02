85a07a4328cd61268fdcc542d13d545513938372
Harish1651999 <hk1651999@gmail.com>
2023-10-02 21:24:30 +0530

    add usefetcher hook.

--- frontend/src/components/NewsletterSignup.js
+++ frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,19 @@
 import classes from "./NewsletterSignup.module.css";
+import { useFetcher } from "react-router-dom";
 
 function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const { data, state } = fetcher;
+
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form method="post" className={classes.newsletter}>
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
