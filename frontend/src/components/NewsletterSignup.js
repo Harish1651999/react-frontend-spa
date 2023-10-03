439c10d90efeb158b6c0e25eacfa77b1dcbc5264
Harish1651999 <hk1651999@gmail.com>
2023-10-03 08:41:24 +0530

    add fetcher destructuring.

--- frontend/src/components/NewsletterSignup.js
+++ frontend/src/components/NewsletterSignup.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import classes from "./NewsletterSignup.module.css";
 import { useFetcher } from "react-router-dom";
 
@@ -5,8 +6,18 @@ function NewsletterSignup() {
   const fetcher = useFetcher();
   const { data, state } = fetcher;
 
+  useEffect(() => {
+    if (state == "idle" && data && data.message) {
+      window.alert(data.message);
+    }
+  }, [data, state]);
+
   return (
-    <fetcher.Form method="post" className={classes.newsletter}>
+    <fetcher.Form
+      method="post"
+      action="/newsletter"
+      className={classes.newsletter}
+    >
       <input
         type="email"
         placeholder="Sign up for newsletter..."
